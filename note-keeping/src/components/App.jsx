7a3b8825563abf922080a8308ec3ca142d11d8e3
Christos Karabatsos <chris.karabatsos5@gmail.com>
2025-06-09 21:35:44 -0400

    Putting all together

--- note-keeping/src/components/App.jsx
+++ note-keeping/src/components/App.jsx
@@ -7,10 +7,39 @@ import Note from './Note';
 
 
 function App() {
+const [notes, setNotes] = useState([]);
+
+    function addNote(newNote) {
+        setNotes(prevNotes => {
+            return [...prevNotes, newNote];
+        });
+    }
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    }
+
+
+
+
     return (
         <div>
             <Header />
-            <CreateArea />
+            <CreateArea onAdd = {addNote}/>
+            {notes.map((noteItem, index) => {
+                return (
+                    <Note
+                        key={index}
+                        id={index}
+                        title={noteItem.title}
+                        content={noteItem.content}
+                        onDelete={deleteNote}
+                    />
+                );
+            })}
             <Footer />
         </div>
     );
